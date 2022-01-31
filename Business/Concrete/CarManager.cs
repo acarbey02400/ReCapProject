931f6c79d2feeb975bf0ea7b82e5fdccc1b53e7d
acarbey02400 <acarwoody01@gmail.com>
2022-02-01 02:24:41 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Absract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+   public class CarManager
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public void Add(Car car)
+        {
+            _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car() { Id = 5, BrandId = 2, ColorId = 1, DailyPrice = 140, ModelYear = 2016, Description = "araba yeğen" };
+
+            CarManager carManager = new CarManager(new CarDal());
+            carManager.Add(car);
+            foreach (Car car1 in carManager.GetAll())
+            {
+                Console.WriteLine(car1.Description);
+            }
+       
+            Console.WriteLine("--------------------\n\n");
+            carManager.Delete(car);
+            foreach (Car car1 in carManager.GetAll())
+            {
+                Console.WriteLine(car1.Description);
+            }
+
+
+        }
+    }
+}
